99f638362600485db48880594d6704d30e8ebfee
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2022-05-22 17:48:43 +0200

    Added JSBasic  challenges 1-9 scope

---
+++ docs/javascript_specialization/javascript_basics/challenges/js/challenges_1_to_9.js
@@ -0,0 +1,29 @@
+(function () {
+  "use strict";
+  const color = prompt("Please, enter a color", "");
+
+  if (color === "") {
+    console.log("You entered an empty string");
+  } else {
+    handleColor(color);
+  }
+
+  function handleColor(enteredColor) {
+    switch (enteredColor.toLowerCase()) {
+      case "red":
+      case "blue":
+      case "yellow":
+        console.log(`You entered a primary color`);
+        break;
+      case "orange":
+      case "green":
+      case "purple":
+        console.log(`You entered a secondary color`);
+        break;
+      default:
+        console.log(`You entered neither a primary or a secondary color`);
+        break;
+    }
+    console.log(`Your picked color was ${enteredColor}`);
+  }
+})();
