bec66d1c1c76c6c4db8ec0e0166c5bba6e75afbc
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2022-05-22 19:28:33 +0200

    Added show next  and previous image  in m3.1 exercise

--- docs/javascript_specialization/javascript_basics/m3.1/js/index.js
+++ docs/javascript_specialization/javascript_basics/m3.1/js/index.js
@@ -3,8 +3,29 @@
   const imageUrl = "images/image";
   const imagesLenght = 5;
   let images = [];
+  let currentImage = 0;
+  let imageTag = document.getElementById("myimage");
+  document.getElementById("next").onclick = showNextImage;
+  document.getElementById("previous").onclick = showPreviousImage;
+
   for (let i = 0; i < imagesLenght; i++) {
     images[i] = `${imageUrl}${i + 1}.jpg`;
     console.log(images[i]);
+  }
+
+  function showNextImage() {
+    currentImage++;
+    if (currentImage > imagesLenght - 1) {
+      currentImage = 0;
+    }
+    imageTag.src = images[currentImage];
+  }
+
+  function showPreviousImage() {
+    currentImage--;
+    if (currentImage === 0) {
+      currentImage = 4;
+    }
+    imageTag.src = images[currentImage];
   }
 })();
