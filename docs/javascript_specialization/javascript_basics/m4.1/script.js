a4023a5b19ada5b925052742a1953cf893e9e5c9
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2022-05-25 22:44:07 +0200

    Changed let conversion to const in js-basic m4.1

--- docs/javascript_specialization/javascript_basics/m4.1/script.js
+++ docs/javascript_specialization/javascript_basics/m4.1/script.js
@@ -10,7 +10,7 @@
   function showAnswer(distance) {
     const anwserContainer = document.getElementById("answer");
     if (distance) {
-      let conversion = (distance * 1.60934).toFixed(2);
+      const conversion = (distance * 1.60934).toFixed(2);
       anwserContainer.innerHTML = `<h2>${distance}mi = ${conversion}km</h2>`;
     } else {
       anwserContainer.innerHTML = "<h2>Please, enter a valid number!</h2>";
