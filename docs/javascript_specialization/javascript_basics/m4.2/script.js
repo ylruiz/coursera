9c06b386a09655680271381873b9cf80c2a40036
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2022-05-26 15:16:28 +0200

    Added js-basic m4.2

---
+++ docs/javascript_specialization/javascript_basics/m4.2/script.js
@@ -0,0 +1,60 @@
+(function () {
+  "use strict";
+  let isMiles = true;
+  const heading = document.querySelector("h1");
+  const intro = document.querySelector("p");
+  const answer = document.getElementById("answer");
+  const form = document.getElementById("convert");
+
+  document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    const isKeyK = key === "k";
+    const isKeyM = key === "m";
+
+    if (isKeyM || isKeyK) {
+      isMiles = isKeyM;
+      const km = "Kilometers";
+      const mi = "Miles";
+      const headingText = `${isMiles ? mi : km} to ${
+        isMiles ? km : mi
+      } Converter`;
+      const introText = `Type in a number of ${
+        isMiles ? mi.toLowerCase() : km.toLowerCase()
+      } and click the button to convert the distance to ${
+        isMiles ? km.toLowerCase() : mi.toLowerCase()
+      }.`;
+
+      heading.innerText = headingText;
+      intro.innerText = introText;
+      showAnswer();
+    }
+  });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    showAnswer();
+  });
+
+  function showAnswer() {
+    let distance = document.getElementById("distance").value;
+    if (parseFloat(distance)) {
+      const conversion = (
+        isMiles ? distance * 1.60934 : distance / 1.60934
+      ).toFixed(2);
+      const answerText = isMiles
+        ? `${distance}mi = ${conversion}km`
+        : `${distance}km = ${conversion}mi`;
+      answer.innerHTML = `<h2>${answerText}</h2>`;
+    } else {
+      answer.innerHTML =
+        "<h2>Please, enter a valid number! It should be different from zero 😉</h2>";
+      setTimeout(function () {
+        console.log(`distance inside setTimeout ${distance}`);
+        answer.innerHTML = '<h2 class="invisible">Answer goes here</h2>';
+        form.reset();
+      }, 3000);
+    }
+  }
+
+  console.log(document.getElementById("distance").value);
+})();
